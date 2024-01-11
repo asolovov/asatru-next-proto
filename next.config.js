71e561a2c1de2b753a297ee56b4d7fd146947f06
asolovov <andre.solovov@gmail.com>
2024-01-11 21:39:31 +0300

    update: og image

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+
+const isProd = process.env.NODE_ENV === 'production';
+
+const nextConfig = {
+    assetPrefix: isProd ? "https://asatru.live" : undefined,
+}
 
 module.exports = nextConfig
