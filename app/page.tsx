6341b89434c51acc80069d58197daeebe13c0481
asolovov <andre.solovov@gmail.com>
2024-01-12 20:35:09 +0300

    feat: audumla & main page updates

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 export default function Home() {
     return (
-        <main className={styles.main}>
+        <main className={"main"}>
             <div className={styles.container}>
                 <h1 className={styles.title}>
                     Что такое Асатру?
@@ -19,23 +19,26 @@ export default function Home() {
                 <p>Корни этой религии лежат в представлениях германских племён эпохи Переселения
                     народов и римского и доримского железного веков. Однако основные
                     источники наших знаний о древней вере дошли до наших дней в исландских рукописях
-                    XII-XIV веков: в поэмах «<Link target={"_blank"} className={styles.link}
+                    XII-XIV веков: в поэмах «<Link target={"_blank"} className={"link"}
                                                    href={"https://norroen.info/src/edda/"}>Старшей
                         Эдды</Link>»,
-                    «<Link target={"_blank"} className={styles.link} href={"https://norroen.info/src/snorra/"}>Младшей
+                    «<Link target={"_blank"} className={"link"} href={"https://norroen.info/src/snorra/"}>Младшей
                         Эдды</Link>»
-                    исландца <Link target={"_blank"} className={styles.link}
+                    исландца <Link target={"_blank"} className={"link"}
                                    href={"https://norroen.info/src/snorra/litmon/stk.html"}>Снорри
                         Стурлусона</Link>
-                    , <Link target={"_blank"} className={styles.link} href={"https://norroen.info/src/abc.html"}>исландских
+                    , <Link target={"_blank"} className={"link"} href={"https://norroen.info/src/abc.html"}>исландских
                         сагах</Link> и
                     стихотворениях скальдов. Поэтому в Асатру
                     используется в основном <b>древнескандинавская</b> терминология, также встречается
-                    использование <b>древнего готского</b> и <b>прагерманского</b> языков.
+                    использование <b>древнего готского</b>, <b>староанглийского</b> и <b>прагерманского</b> языков.
                     Помимо современного термина «Асатру» нередко применяются названия наподобии
                     исландского <b>Forn Siðr</b> («Древний обычай») или английского <b>Troth</b> («Верность», «Истина»)
                     и им подобных. Асатру не имеет священных книг. Такие литературные источники, как
-                    «Старшая Эдда» и «Младшая Эдда» являются наиболее авторитетными, но не
+                    «<Link target={"_blank"} className={"link"}
+                           href={"https://norroen.info/src/edda/"}>Старшая
+                        Эдда</Link>» и «<Link target={"_blank"} className={"link"} href={"https://norroen.info/src/snorra/"}>Младшая
+                        Эдда</Link>» являются наиболее авторитетными, но не
                     рассматриваются как священные или боговдохновенные.</p>
             </div>
             <h2 className={styles.sbuTitle}>
@@ -47,18 +50,13 @@ export default function Home() {
                     возрождения древней веры) стал 1972 год, когда
                     исландец <Link
                         target={"_blank"}
-                        className={styles.link}
+                        className={"link"}
                         href={"https://en.wikipedia.org/wiki/Sveinbj%C3%B6rn_Beinteinsson"}>Свейнбьерн Бейнтейнссон</Link> основал
                     первую общину Асатру - <Link
                         target={"_blank"}
-                        className={styles.link}
+                        className={"link"}
                         href={"https://asatru.is/"}>Ásatrúarfélagið</Link> в
                     Исландии. С 1973 году Асатру в этой стране существует как легальная религия.</p>
-                <p>В том же 1973 году Стивен Макналлен основывал Свободное Собрание Асатру в США. В
-                    1980 г. известный исследователь Эдред Торссон основывал международную организацию
-                    «Рунная Гильдия». В 1988 году в США создается «The Troth» (Трот), ныне
-                    представляющая собой значительное и разветвлённое объединение. В период с 1994 по
-                    1999 год организации Асатру возникают в скандинавских странах.</p>
                 <p>В настоящее время организации Асатру существуют практически во всех странах
                     мира. Асатру не определяет политических воззрений своих последователей, поэтому
                     среди исповедующих его встречается весь спектр политических предпочтений. Однако
@@ -84,123 +82,163 @@ export default function Home() {
                 <p>Последователи асатру чтут богов скандинавского пантеона
                     — <Link
                         target={"_blank"}
-                        className={styles.link}
+                        className={"link"}
                         href={"https://norroen.info/wiki/%D0%90%D1%81%D1%8B"}>асов</Link> и <Link
                         target={"_blank"}
-                        className={styles.link}
+                        className={"link"}
                         href={"https://norroen.info/wiki/%D0%92%D0%B0%D0%BD%D1%8B"}>ванов</Link>, богинь судьбы
                     — <Link
                         target={"_blank"}
-                        className={styles.link}
+                        className={"link"}
                         href={"https://norroen.info/wiki/%D0%9D%D0%BE%D1%80%D0%BD%D1%8B"}>норн</Link>,
                     младших божеств
                     — <Link
                         target={"_blank"}
-                        className={styles.link}
+                        className={"link"}
                         href={"https://norroen.info/wiki/%D0%92%D0%B0%D0%BB%D1%8C%D0%BA%D0%B8%D1%80%D0%B8%D0%B8"}>валькирий</Link>, духов
                     местности с общим названием ланветты (landvættr), а также предков по мужской линии
                     — <Link
                         target={"_blank"}
-                        className={styles.link}
+                        className={"link"}
                         href={"https://norroen.info/wiki/%D0%90%D0%BB%D1%8C%D0%B2%D1%8B"}>альвов</Link>,
                     и по женской
                     — <Link
                         target={"_blank"}
-                        className={styles.link}
+                        className={"link"}
                         href={"https://norroen.info/wiki/%D0%94%D0%B8%D1%81%D1%8B"}>дис</Link>.
                     Часть последователей почитает также древних существ,
                     именуемых <Link
                         target={"_blank"}
-                        className={styles.link}
+                        className={"link"}
                         href={"https://norroen.info/wiki/%D0%81%D1%82%D1%83%D0%BD%D1%8B"}>ётунами</Link>. Иногда
                     одновременно почитаются те или иные божества, известные из пантеона древних
                     германцев (Нертус, Нехаления, Танфана и т.д.). </p>
-                <p>Основными богами являются глава асов и бог мудрости, войны и
+                <p>Основными богами являются глава <Link
+                    target={"_blank"}
+                    className={"link"}
+                    href={"https://norroen.info/wiki/%D0%90%D1%81%D1%8B"}>асов</Link> и бог мудрости, войны и
                     вдохновения <Link
                         target={"_blank"}
-                        className={styles.link}
+                        className={"link"}
                         href={"https://norroen.info/wiki/%D0%9E%D0%B4%D0%B8%D0%BD"}>Один</Link>, его
                     жена <Link
                         target={"_blank"}
-                        className={styles.link}
+                        className={"link"}
                         href={"https://norroen.info/wiki/%D0%A4%D1%80%D0%B8%D0%B3%D0%B3"}>Фригг</Link>, бог громовержец и
                     защитник <Link
                         target={"_blank"}
-                        className={styles.link}
+                        className={"link"}
                         href={"https://norroen.info/wiki/%D0%A2%D0%BE%D1%80"}>Тор</Link> и его
                     жена <Link
                         target={"_blank"}
-                        className={styles.link}
+                        className={"link"}
                         href={"https://norroen.info/wiki/%D0%A1%D0%B8%D0%B2"}>Сив</Link>,
                     бог моря и
                     богатства <Link
                         target={"_blank"}
-                        className={styles.link}
+                        className={"link"}
                         href={"https://norroen.info/wiki/%D0%9D%D1%8C%D1%91%D1%80%D0%B4"}>Ньёрд</Link> и его дети, бог
                     плодородия <Link
                         target={"_blank"}
-                        className={styles.link}
+                        className={"link"}
                         href={"https://norroen.info/wiki/%D0%A4%D1%80%D0%B5%D0%B9%D1%80"}>Фрейр</Link> и богиня
                     любви <Link
                         target={"_blank"}
-                        className={styles.link}
+                        className={"link"}
                         href={"https://norroen.info/wiki/%D0%A4%D1%80%D0%B5%D0%B9%D1%8F"}>Фрейя</Link>, почитаются бог
                     чести и
                     доблести <Link
                         target={"_blank"}
-                        className={styles.link}
+                        className={"link"}
                         href={"https://norroen.info/wiki/%D0%A2%D1%8E%D1%80"}>Тюр</Link>, страж
                     Асгарда <Link
                         target={"_blank"}
-                        className={styles.link}
+                        className={"link"}
                         href={"https://norroen.info/wiki/%D0%A5%D0%B5%D0%B9%D0%BC%D0%B4%D0%B0%D0%BB%D0%BB%D1%8C"}>Хеймдалль</Link>,
                     Форсетти, бог закона и
                     справедливого суда
                     , <Link
                         target={"_blank"}
-                        className={styles.link}
+                        className={"link"}
                         href={"https://norroen.info/wiki/%D0%98%D0%B4%D1%83%D0%BD%D0%BD"}>Идунн</Link>,
                     богиня молодости и обновления, скальд
                     богов <Link
                         target={"_blank"}
-                        className={styles.link}
+                        className={"link"}
                         href={"https://norroen.info/wiki/%D0%91%D1%80%D0%B0%D0%B3%D0%B8"}>Браги</Link>, зимняя
                     охотница <Link
                         target={"_blank"}
-                        className={styles.link}
+                        className={"link"}
                         href={"https://norroen.info/wiki/%D0%A1%D0%BA%D0%B0%D0%B4%D0%B8"}>Скади</Link>, бог-
                     лучник Улль и т.д. </p>
-                <p>Поскольку понятие «ас» (Ás) является не только названием группы богов-асов, но и
+                <p>Поскольку понятие «<Link
+                    target={"_blank"}
+                    className={"link"}
+                    href={"https://norroen.info/wiki/%D0%90%D1%81%D1%8B"}>ас</Link>» (Ás) является не только названием группы богов-<Link
+                    target={"_blank"}
+                    className={"link"}
+                    href={"https://norroen.info/wiki/%D0%90%D1%81%D1%8B"}>асов</Link>, но и
                     используется в источниках в значении «бог», выделение почитания одного или группы сверхъестественных
-                    существ (отдельно
-                    ванов или отдельно ётунов и т.д.) тоже попадает под определение Асатру в широком
+                    существ (отдельно <Link
+                        target={"_blank"}
+                        className={"link"}
+                        href={"https://norroen.info/wiki/%D0%92%D0%B0%D0%BD%D1%8B"}>ванов</Link> или отдельно <Link
+                        target={"_blank"}
+                        className={"link"}
+                        href={"https://norroen.info/wiki/%D0%81%D1%82%D1%83%D0%BD%D1%8B"}>ётунов</Link> и т.д.) тоже попадает под определение Асатру в широком
                     смысле. Тем не менее, в частных случаях используются иногда термины самоопределения
                     «ванатру», «рёккатру» и т.д. Распространённый в западных странах зонтичный термин
                     heathen
                     или <Link
                         target={"_blank"}
-                        className={styles.link}
+                        className={"link"}
                         href={"https://en.wikipedia.org/wiki/Heathenry_(new_religious_movement)"}>Heathenry</Link> в русскоязычном
                         коммьюнити используется редко.</p>
                 <p>Большая часть последователей Асатру верит в создание Вселенной в результате взаимодействия огненного и ледяного начал,
                     и в создание известного людям мира из тела
                     Первосущества <Link
                         target={"_blank"}
-                        className={styles.link}
+                        className={"link"}
                         href={"https://norroen.info/wiki/%D0%98%D0%BC%D0%B8%D1%80"}>Имира</Link>, в то, что боги либо
                     создали первых людей, либо придали им человеческий облик и
                     наделили душой и разумом, в то, что судьбу определяют или олицетворяют богини
-                    судьбы – норны, и их приговор непреложен. Судьба мироздания, в целом,
+                    судьбы – <Link
+                        target={"_blank"}
+                        className={"link"}
+                        href={"https://norroen.info/wiki/%D0%9D%D0%BE%D1%80%D0%BD%D1%8B"}>норны</Link>, и их приговор непреложен. Судьба мироздания, в целом,
                     предопределена, и известный нам мир погибнет при Рагнарёке, Последней битве богов с хтоническими
                     чудовищами.
-                    В этой битве погибнет глава богов Один, боги Тор, Тюр, Хеймдалль, Фрейр. Однако их жертва и борьба
-                    спутников Одина — эйнхериев — позволит миру и людям в нём возродиться для нового Золотого Века (с
+                    В этой битве погибнет глава богов <Link
+                        target={"_blank"}
+                        className={"link"}
+                        href={"https://norroen.info/wiki/%D0%9E%D0%B4%D0%B8%D0%BD"}>Один</Link>, боги <Link
+                        target={"_blank"}
+                        className={"link"}
+                        href={"https://norroen.info/wiki/%D0%A2%D0%BE%D1%80"}>Тор</Link>, <Link
+                        target={"_blank"}
+                        className={"link"}
+                        href={"https://norroen.info/wiki/%D0%A2%D1%8E%D1%80"}>Тюр</Link>, <Link
+                        target={"_blank"}
+                        className={"link"}
+                        href={"https://norroen.info/wiki/%D0%A5%D0%B5%D0%B9%D0%BC%D0%B4%D0%B0%D0%BB%D0%BB%D1%8C"}>Хеймдалль</Link>, <Link
+                        target={"_blank"}
+                        className={"link"}
+                        href={"https://norroen.info/wiki/%D0%A4%D1%80%D0%B5%D0%B9%D1%80"}>Фрейр</Link>. Однако их жертва и борьба
+                    спутников <Link
+                        target={"_blank"}
+                        className={"link"}
+                        href={"https://norroen.info/wiki/%D0%9E%D0%B4%D0%B8%D0%BD"}>Одина</Link> — эйнхериев — позволит миру и людям в нём возродиться для нового Золотого Века (с
                     которого некогда началась история богов).</p>
                 <p>Вопросы посмертия не являются для большинства последователей Асатру определяющими, но, в целом,
-                    считается, что достойно павшие в бою отправляются в чертог Одина — Вальхаллу, верные тому или иному
+                    считается, что достойно павшие в бою отправляются в чертог <Link
+                        target={"_blank"}
+                        className={"link"}
+                        href={"https://norroen.info/wiki/%D0%9E%D0%B4%D0%B8%D0%BD"}>Одина</Link> — Вальхаллу, верные тому или иному
                     богу могут попасть в его жилище, те
-                    же, кто не был удостоен выбора богов, отправляются во владения ётунши Хель, поставленной
-                    Одином хранить мир мёртвых. Также в Асатру признаётся возможность возрождения
+                    же, кто не был удостоен выбора богов, отправляются во владения ётунши Хель, поставленной <Link
+                        target={"_blank"}
+                        className={"link"}
+                        href={"https://norroen.info/wiki/%D0%9E%D0%B4%D0%B8%D0%BD"}>Одином</Link> хранить мир мёртвых. Также в Асатру признаётся возможность возрождения
                     умершего в роду, упомянутая в древних источниках.
                     Спектр теологических объяснений природы богов и мироздания среди последователей
                     Асатру крайне широк, от анимизма и классического политеизма — до неоплатонизма и
@@ -234,10 +272,16 @@ export default function Home() {
             </h2>
             <hr/>
             <div className={styles.mainText}>
-                <p>Основными символами в Асатру является Молот Тора (Мьёльнир) в виде подвесок для
+                <p>Основными символами в Асатру является Молот <Link
+                    target={"_blank"}
+                    className={"link"}
+                    href={"https://norroen.info/wiki/%D0%A2%D0%BE%D1%80"}>Тора</Link> (Мьёльнир) в виде подвесок для
                     личного ношения или в виде символического изображения молота для проведения
                     обрядов, а также так называемое «кольцо клятв» — разомкнутый браслет, на котором
-                    приносятся клятвы именем богов. Также распространена символика копья, оружия Одина,
+                    приносятся клятвы именем богов. Также распространена символика копья, оружия <Link
+                        target={"_blank"}
+                        className={"link"}
+                        href={"https://norroen.info/wiki/%D0%9E%D0%B4%D0%B8%D0%BD"}>Одина</Link>,
                     воронов, его спутников, изображения валькнута (три переплетённых треугольника) и
                     отдельных рун, круг с крестом внутри, заимствованный со скандинавских петроглифов
                     бронзового века и т.д. Символ так называемого «Чёрного солнца» (заимствованный
@@ -267,7 +311,10 @@ export default function Home() {
                     его ограды, физической или символической.</p>
                 <p>Хотя значительная часть общин Асатру в мире существует вне связи с какими-либо
                     объединениями, есть также разветвлённые организации, объединяющие своих членов в на
-                    значительных территориях. Таковы исландская Ásatrúarfélagið, датская Forn Siðr (Asa- og
+                    значительных территориях. Таковы исландская  <Link
+                        target={"_blank"}
+                        className={"link"}
+                        href={"https://asatru.is/"}>Ásatrúarfélagið</Link>, датская Forn Siðr (Asa- og
                     Vanetrosamfundet i Danmark), шведская Nordiska Asa-samfundet, американские Asatru Folk
                     Assambley и The Troth и т.д.</p>
                 <p>Первые объединения последователей Асатру  на территории СНГ появились в 2004
